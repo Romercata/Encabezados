5e9aeb683a2838afbc05dd9202dd049644a530c5
romer <romer@192.168.0.9>
2021-03-23 21:03:06 -0400

    Update

---
+++ src/java/com/emergentes/RespJSON.java
@@ -0,0 +1,35 @@
+package com.emergentes;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "RespJSON", urlPatterns = {"/RespJSON"})
+public class RespJSON extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.setHeader("Content-Disposition", "attachment;filename=empleados.json");
+        PrintWriter out = response.getWriter();
+        try{
+           
+            out.println("{'empleados':[");
+            out.println("{'nombre' : 'Bruno','apellidos':'Diaz'}");
+            out.println("<head>");
+            out.println("{'nombre':'Juan Jose','apellidos':'Paredes'}");            
+            out.println("{'nombre':'Dolores','apellidos':'Fuentes'}");
+            out.println("<body>");
+            out.println("<h1>Servlet RespJSON at " + request.getContextPath() + "</h1>");
+            out.println("]}");
+        }finally{
+            out.close();
+        }
+    }
+
+}
